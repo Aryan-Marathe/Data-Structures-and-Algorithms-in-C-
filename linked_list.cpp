3435d0365440ae185befc0bb7760916df6eb7d6d
Aryan Marathe <aryanmarathe1103@gmail.com>
2026-04-04 11:53:16 +0530

    4/3/2025

--- linked_list.cpp
+++ linked_list.cpp
@@ -40,6 +40,17 @@ void linked_list_traversal(node* head){
     }
 }
 
+int size_LL(node* head){
+    int cnt=0;
+    node* temp=head;
+    while (temp!=nullptr)
+    {
+        cnt++;
+        temp=temp->next;
+    }
+    return cnt;  
+}
+
 node* add_numbers_in_LL(node* head1,node* head2){
 
     node* dummyNode=new node(-1);
@@ -119,9 +130,49 @@ node* odd_even_LL(node* head){
     odd->next=evenhead;
     return head;
 } 
+
+node* sort_LL_of_0_1_2(node* head){
+
+    if(head==nullptr || head->next==nullptr){
+        return head;
+    }
+    node* dummynode0=new node(-1);
+    node* curr0=dummynode0;
+    node* dummynode1=new node(-1);
+    node* curr1=dummynode1;
+    node* dummynode2=new node(-1);
+    node* curr2=dummynode2;
+    node* temp=head;
+
+    while (temp!=nullptr)
+    {
+        if(temp->data==0){
+            curr0->next=temp;
+            curr0=temp;
+        }
+        else if(temp->data==1){
+            curr1->next=temp;
+            curr1=temp;
+        }
+        else{
+            curr2->next=temp;
+            curr2=temp;
+        }
+        temp=temp->next;
+    }
+    curr0->next=(dummynode1->next)?dummynode1->next:dummynode2->next;
+    curr1->next=dummynode2->next;
+    curr2->next=nullptr;
+    return dummynode0->next;
+}
+
+node* remove_nth_node_from_the_back(node* head,int n){
+
+    
+}
 int main(){
     vector<int> arr={2 ,5, 6, 8, 9, 1};
-    vector<int> arr2={0,0,1};
+    vector<int> arr2={0,0,1,2,1,0,2,1,0};
     node* head1=convert_array_to_linked_list(arr);
     node* head2=convert_array_to_linked_list(arr2);
 
@@ -130,6 +181,12 @@ int main(){
 
     // node* head=odd_even_LL(head1);
     // linked_list_traversal(head);
+
+    // node* head=sort_LL_of_0_1_2(head2);
+    // linked_list_traversal(head);
+
+    node* head=remove_nth_node_from_the_back(head,3);
+    linked_list_traversal(head);
 
     return 0;
 }
