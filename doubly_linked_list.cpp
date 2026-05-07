45721164675d6b3e682cc8084bb4a904ba171fa4
Aryan Marathe <aryanmarathe1103@gmail.com>
2026-05-07 13:04:03 +0530

    7/5/26

--- doubly_linked_list.cpp
+++ doubly_linked_list.cpp
@@ -50,8 +50,7 @@ Dnode* reverse_DLL(Dnode* head){
 
     if(head==nullptr || head->next==nullptr){
         return head;
-    }
-    
+    }    
     else {
         Dnode* front=head;
         Dnode* back=head;
@@ -66,15 +65,85 @@ Dnode* reverse_DLL(Dnode* head){
             back=back->prev;
         }  
          return head; 
+    }    
+}
+
+Dnode* delete_all_ocuurances_of_key(Dnode* head,int key){
+
+      Dnode* temp=head;
+      while (temp!=nullptr)
+      {
+        if(temp->data==key){
+            if(temp==head){
+                head=head->next;
+            }
+            Dnode* nextNode=temp->next;
+            Dnode* prevNode=temp->prev;
+            if(nextNode){
+                nextNode->prev=prevNode;
+            }
+            if(prevNode){
+                prevNode->next=nextNode;
+            }
+            delete temp;
+            temp=nextNode;
+        }
+        else{
+            temp=temp->next;
+        }
+      }
+      return head;
+}
+
+void find_pairs_with_given_sum(Dnode* head,int sum){
+   
+    if(head==nullptr || head->next==nullptr){
+        cout<<0;
     }
+   vector<pair<int,int>> vec;
+   Dnode* front=head;
+   Dnode* back=head;
+   
+   while (back->next != nullptr)
+   {
+    back=back->next;
+   }
+   
+   while (front!=back &&front->prev!=back)
+   {
+    int val_sum=front->data+back->data;
+    if(val_sum==sum){
+        vec.push_back({front->data,back->data});
+    }
+    else if(val_sum>sum){
+        back=back->prev;
+    }
+    else{
+        front=front->next;
+    }
+   }
+   for ( auto it : vec)
+   {
+    cout<<it.first<<" "<<it.second<<endl;
+   }
+   
+}
+
+Dnode* remove_duplicates_from_sortedDLL(Dnode* head){
 
     
 }
 int main(){
-    vector<int> arr={2 ,5, 6, 8, 9, 1};
+    vector<int> arr={1,2,3,4,9};
     Dnode* head=create_DLL(arr);
 
-    head=reverse_DLL(head);
-    DLL_traversal(head);
+    // head=reverse_DLL(head);
+    // DLL_traversal(head);
+
+    // head=delete_all_ocuurances_of_key(head,9);
+    // DLL_traversal(head);
+
+    find_pairs_with_given_sum(head,5);
+    
      return 0;
 }
