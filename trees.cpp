318a5f5090a10e86597e9768331df640c9253a25
Aryan Marathe <aryanmarathe1103@gmail.com>
2026-05-18 14:00:25 +0530

    18/5/26

--- trees.cpp
+++ trees.cpp
@@ -58,7 +58,115 @@ node* inorder(node* root){
     inorder(root->right);   
 }
 
+vector<vector<int>> level_order(node* root){
+    vector<vector<int>> ans;
+    if(root==nullptr){
+        return ans;
+    }
+    queue<node*> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        int size=q.size();
+        vector<int> level;
+        for(int i=0;i<size;i++){
+            node* root=q.front();
+            q.pop();
+            if(root->left!=nullptr){
+                q.push(root->left);
+            }
+            if(root->right){
+                q.push(root->right);
+            }
+            level.push_back(root->data);
+        }
+        ans.push_back(level);
+    }
+    return ans;
+}
 
+void pre_order_iterative(node* root){
+    if(root==nullptr){
+        return;
+    }
+    stack<node*> st;
+    st.push(root);
+    while (!st.empty())
+    {
+        node* curr=st.top();
+        st.pop();
+        cout<<curr->data<<" ";
+        if(curr->right!=nullptr){
+            st.push(curr->right);
+        }
+        if(curr->left!=nullptr){
+            st.push(curr->left);
+        }
+    }
+}
+
+int max_depth(node* root){
+    if(root==nullptr){
+        return 0;
+    }
+    int left_height=max_depth(root->left);
+    int right_height=max_depth(root->right);
+    return 1+max(left_height,right_height);
+}
+
+int check_height(node* root){
+    if(root==nullptr){
+        return 0;
+    }
+    int left_height=check_height(root->left);
+    if(left_height==-1) {
+        return -1;
+    }
+    int right_height=check_height(root->right);
+    if(right_height==-1) {
+        return -1;
+    }
+    if(abs(left_height-right_height)>1){
+        return -1;
+    }
+    return 1+max(left_height,right_height);
+}
+
+bool check_balanced_tree(node* root){
+
+    /* the tree called balance binary tree when,
+            
+        height(left_sun_tree)-height(right_sun_tree)<=1
+    */
+
+    if(check_height(root)==-1){
+        return false;
+    }
+    else{
+        return true;
+    }
+
+}
+
+int diameter=0;
+int check_depth(node* root){
+    if(root==nullptr){
+        return 0;
+    }
+
+    int lh=check_depth(root->left);
+    int rh=check_depth(root->right);
+
+    diameter=max(diameter,lh+rh);
+    return 1+max(lh,rh);
+}
+int find_diameter(node* root){
+
+    // diameter of the tree is longest path from one node to another
+
+    check_depth(root);
+    return diameter;
+}
 int main(){
  
     vector<int> arr={1,4,7,3,8,2,9,0};
@@ -70,5 +178,18 @@ int main(){
     // postorder(root);
 
     // inorder(root);
+
+    // level_order(root);
+
+    // pre_order_iterative(root);
+
+    // cout<<max_depth(root);
+
+    // cout<<check_balanced_tree(root);
+
+    cout<<find_diameter(root);
+
     return 0;
 }
+
+
